1bdd7099c595d03b76bb3568e6ec584fc1b88094
Octávio Marques <octavio12@live.com.pt>
2020-12-14 13:07:28 +0000

    * Added labels and URL to components; * Added ngFor;

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -5,7 +5,20 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
+
 export class MenuComponent implements OnInit {
+
+  /*
+  * Menu Routing Buttons Configurations:
+  * * Label: HTML that's showned on button name;
+  * * URL: Route on click event;
+  */
+  menuLinks = [
+    {label:"Who Am I", url:"/who-am-i"},
+    {label:"Education", url:"/education"},
+    {label:"Projects", url:"/projects"},
+    {label:"Skills", url:"/skills"}
+  ]
 
   constructor() { }
 
--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -7,6 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillsComponent implements OnInit {
 
+  /*
+  * Skill List:
+  * * label: Text showned on HTML;
+  * * imgURL: Path to skill Image;
+  */
+  skills = [
+    {label:"Java", imgURL:"../../assets/images/icon/avaluation_med.png"},
+    {label:"Python", imgURL:"../../assets/images/icon/avaluation_low.png"},
+    {label:"Javascript", imgURL:"../../assets/images/icon/avaluation_med.png"},
+    {label:"C++", imgURL:"../../assets/images/icon/avaluation_starter.png"},
+    {label:"PHP", imgURL:"../../assets/images/icon/avaluation_starter.png"},
+    {label:"SQL", imgURL:"../../assets/images/icon/avaluation_med.png"},
+    {label:"Angular", imgURL:"../../assets/images/icon/avaluation_low.png"},
+    {label:"CSS", imgURL:"../../assets/images/icon/avaluation_med.png"}
+  ]
+
+  /*
+  * Back Button Image path URL:
+  */
+  backBtnURL = "../../assets/images/icon/go-back-arrow.png";
+
   constructor() { }
 
   ngOnInit(): void {
